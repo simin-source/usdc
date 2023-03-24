999ad95af28e245aab3f01fecf57a731819aa678
simin-source <2420117338@qq.com>
2023-03-24 22:49:47 +0800

    提交打包内容

--- src/pages/investment/main.ts
+++
@@ -1,6 +0,0 @@
-import '@/assets/style/base.scss';
-import 'element-plus/dist/index.css';
-import { createApp } from 'vue';
-import Index from './Index';
-
-createApp(Index).mount('#root');
--- src/source.d.ts
+++
@@ -1,6 +0,0 @@
-declare const React: string
-declare module '*.json'
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.scss'
-declare module '*.css'
